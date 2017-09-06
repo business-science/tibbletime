aa23d5aab1b3cd3bae93ba40a8d52316088ac815
DavisVaughan <mvaugh15@uncc.edu>
2017-09-06 09:26:59 -0400

    tbl_time documentation

--- R/tbl_time.R
+++ R/tbl_time.R
@@ -1,12 +1,23 @@
-#' Convert an object into a `tbl_time` object
+#' Create `tbl_time` objects
 #'
-#' `tbl_time` objects have a time index that contains information about which column should be
-#' used for time based subsetting and other time based manipulation. Otherwise, they function
-#' as normal tibbles.
+#' `tbl_time` objects have a time index that contains information about
+#' which column should be used for time based subsetting and other time based
+#' manipulation. Otherwise, they function as normal tibbles.
 #'
-#' @param x stuff!!
-#' @param index stuff!!
-#' @param ... Currently not used
+#' @details
+#'
+#' The information stored about `tbl_time` objects are the `index` and the
+#' `time_zone`. These are stored as attributes, with the `index` as a
+#' _quosure_ and the `time_zone` as a string.
+#'
+#' Currently, only the `Date` and `POSIXct` classes are supported to be
+#' time indices.
+#'
+#' @param x An object to be converted to `tbl_time`. This is generally
+#' a [tibble::tibble()], or an object that can first be coerced to a `tibble`.
+#' @param index The bare column name of the column to be used as the index.
+#' @param ... Arguments passed to [tibble::as_tibble()] if coercsion is
+#' necessary first.
 #'
 #' @rdname tbl_time
 #'
@@ -14,12 +25,25 @@
 #'
 #' @examples
 #'
+#' # Converting a data.frame to a `tbl_time`
+#' # Using Date index
+#' ex1 <- data.frame(date = Sys.Date(), value = 1)
+#' ex1_tbl_time <- as_tbl_time(ex1, date)
+#' class(ex1_tbl_time)
+#' attributes(ex1_tbl_time)
+#'
+#' # Converting a tibble to a `tbl_time`
+#' # Using POSIXct index
+#' ex2 <- tibble::tibble(
+#'   time  = as.POSIXct(c("2017-01-01 10:12:01", "2017-01-02 12:12:01")),
+#'   value = c(1, 2)
+#' )
+#' as_tbl_time(ex2, time)
 #'
 tbl_time <- function(x, index, ...) {
   index <- rlang::enquo(index)
   as_tbl_time(x, !! index, ...)
 }
-
 
 #' @export
 #' @rdname tbl_time
@@ -30,12 +54,11 @@ as_tbl_time <- function(x, index, ...) {
 #' @export
 as_tbl_time.default <- function(x, index, ...) {
   index <- rlang::enquo(index)
-  as_tbl_time(tibble::as_tibble(x), !! index, ...)
+  as_tbl_time(tibble::as_tibble(x, ...), !! index)
 }
 
-
 #' @export
-as_tbl_time.data.frame <- function(x, index, ...) {
+as_tbl_time.tbl_df <- function(x, index, ...) {
 
   # Capture index
   index <- rlang::enquo(index)
@@ -62,12 +85,12 @@ as_tbl_time.grouped_df <- function(x, index, ...) {
   # Capture index
   index <- rlang::enquo(index)
 
-  x <- as_tbl_time.data.frame(x, !! index)
+  # Normal conversion plus added class
+  x <- as_tbl_time.tbl_df(x, !! index)
   class(x) <- unique(c("grouped_tbl_time", class(x)))
 
   x
 }
-
 
 # Utils ----
 
--- tests/testthat/test_tbl_time.R
+++ tests/testthat/test_tbl_time.R
@@ -3,16 +3,25 @@ context("tbl_time creation")
 # Test objects
 
 test_time <- tibble::tibble(
+  group = c("g1", "g1", "g2"),
   date  = c(as.Date("2017-12-01"), as.Date("2017-12-02"), as.Date("2017-12-03")),
   value = c(1, 2, 3)
 )
 
-test_tbl_time <- as_tbl_time(test_time, date)
+test_tbl_time_base <- tbl_time(test_time, date)
+test_tbl_time      <- as_tbl_time(test_time, date)
+test_tbl_time_g    <- as_tbl_time(dplyr::group_by(test_time, group), date)
 
 # Tests
 
 test_that("tbl_time class is created", {
-  expect_is(test_tbl_time, "tbl_time")
+  expect_is(test_tbl_time,      "tbl_time")
+  expect_is(test_tbl_time_base, "tbl_time")
+  expect_is(test_tbl_time_g,    "tbl_time")
+})
+
+test_that("grouped_tbl_time class is created when converting a grouped_df", {
+  expect_is(test_tbl_time_g, "grouped_tbl_time")
 })
 
 test_that("tbl_time index is created", {
