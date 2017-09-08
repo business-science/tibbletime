74dcd6762b7c0d58fdb7d6e90ee52b5b2c48f824
DavisVaughan <mvaugh15@uncc.edu>
2017-09-08 10:13:43 -0400

    Added package doc page. tibble version down to 1.3.3

--- NEWS.md
+++ NEWS.md
@@ -1,8 +1,15 @@
 ## tibbletime 0.0.1.9000
 
-* Add versions to all imported packages
+* General
 
-* Fix a bug with as.Date / as.POSIXct operator collision in `time_filter()`.
+    * Added package documentation page
+
+    * Added versions to all imported packages
+
+
+* Bug Fixes
+
+    * Fix a bug with as.Date / as.POSIXct operator collision in `time_filter()`.
 
 ## tibbletime 0.0.1 
 
---
+++ R/tibbletime-package.r
@@ -0,0 +1,12 @@
+#' tibbletime: time-aware tibbles
+#'
+#' Built on top of the 'tibble' package, 'tibbletime' is an extension
+#' that allows for the creation of time aware tibbles. Some immediate
+#' advantages of this include: the ability to perform time based subsetting
+#' on tibbles, quickly summarising and aggregating results by time periods,
+#' and calling functions similar in spirit to the map family from 'purrr'
+#' on time based tibbles.
+#'
+#' @name tibbletime
+#' @docType package
+"_PACKAGE"
