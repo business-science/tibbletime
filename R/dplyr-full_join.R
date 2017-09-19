d2ca7794dd18a75fff0afd712b35913442f99649
DavisVaughan <mvaugh15@uncc.edu>
2017-09-19 16:00:09 -0400

    Add support for full_join.tbl_time

---
+++ R/dplyr-full_join.R
@@ -0,0 +1,9 @@
+#' @export
+#' @importFrom dplyr full_join
+#'
+full_join.tbl_time <- function(x, y, by = NULL, copy = FALSE, suffix = c(".x", ".y"), ...) {
+  tidyverse_execute(x, full_join, y = y, by = by, copy = copy, suffix = suffix, ...)
+}
+
+#' @export
+dplyr::full_join
