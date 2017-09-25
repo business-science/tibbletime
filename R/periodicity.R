53f22191eb9f3838c5841a361ac8a3a6860f61ff
DavisVaughan <mvaugh15@uncc.edu>
2017-09-25 19:05:04 -0400

    Move period_to_syms to utils

--- R/periodicity.R
+++ R/periodicity.R
@@ -107,21 +107,3 @@ as_period.tbl_time <- function(x, period = "yearly", side = "start") {
     dplyr::distinct(!! index_name, .keep_all = TRUE)
 
 }
-
-# Utils ----
-
-period_to_syms <- function(period) {
-  groups <- switch (period,
-          "yearly"     = list("year"),
-          "quarterly"  = list("year", "quarter"),
-          "monthly"    = list("year", "month"),
-          "weekly"     = list("year", "month", "week"),
-          "daily"      = list("year", "month", "day"),
-          "hourly"     = list("year", "month", "day", "hour"),
-          "minute"     = list("year", "month", "day", "hour", "minute"),
-          "second"     = list("year", "month", "day", "hour", "minute", "second"),
-          stop("`period` is not one of: 'yearly', 'quarterly', 'monthly', 'weekly', 'daily', 'hourly', 'minute', 'second'", call. = FALSE)
-  )
-
-  rlang::syms(groups)
-}
--- R/utils.R
+++ R/utils.R
@@ -6,3 +6,23 @@ magrittr::`%>%`
 #' @importFrom rlang :=
 #'
 NULL
+
+# Convert a period to an expanded list of syms required to expand the index
+period_to_syms <- function(period) {
+  groups <- switch (period,
+      "yearly"     = list("year"),
+      "quarterly"  = list("year", "quarter"),
+      "monthly"    = list("year", "month"),
+      "weekly"     = list("year", "month", "week"),
+      "daily"      = list("year", "month", "day"),
+      "hourly"     = list("year", "month", "day", "hour"),
+      "minute"     = list("year", "month", "day", "hour", "minute"),
+      "second"     = list("year", "month", "day", "hour", "minute", "second"),
+      stop("`period` is not one of: 'yearly', 'quarterly', 'monthly', 'weekly',
+           'daily', 'hourly', 'minute', 'second'", call. = FALSE)
+  )
+
+  rlang::syms(groups)
+}
+
+
