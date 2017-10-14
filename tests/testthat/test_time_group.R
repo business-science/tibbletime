74c6b47e4afa0fef9526b26d143176148f0bc539
dmi3kno <dperepolkin@yahoo.no>
2017-10-14 15:26:22 +0200

    Fix #22 with time_group() reshuffling index using tibble approach and add relevant test

---
+++ tests/testthat/test_time_group.R
@@ -0,0 +1,31 @@
+context("time_group testing")
+# time_group(index, period = "yearly", start_date = NULL, ...)
+
+# Test objects
+
+data(FB)
+test_index <-FB$date
+test_index_tbl <-FB %>% dplyr::select(date)
+
+data(FANG)
+test_index_g <- FANG$date
+test_index_tbl_g <- FANG %>% dplyr::select(date)
+
+# Tests
+
+# is this, btw normal behaviour for FANG? What if it is run on ungrouped data?
+#test_that("early return triggered and seq_along is returned", {
+#  test <- time_group(index=test_index_g, period="hour")
+#  expect_equal(test,  seq_along(test_index_g))
+#})
+
+test_that("Weekly period counting works", {
+  test <- time_group(index=test_index_g, period = "weekly")
+  test_filter_yw <- test_index_tbl_g %>%
+    mutate(year_week=paste(isoyear(date), isoweek(date), sep="-"))
+  test_filter <- test_filter_yw %>% group_by(year_week) %>%
+    summarise(date=max(date)) %>% arrange(date) %>%
+    mutate(group_num=1:n()) %>% select(-date) %>%
+      right_join(test_filter_yw, by="year_week") %>% pull(group_num)
+  expect_equal(test, test_filter)
+})
