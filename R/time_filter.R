26e6c41d8df39dc307784cc4397a6dab9a4b93a0
DavisVaughan <mvaugh15@uncc.edu>
2017-12-21 14:59:11 -0500

    time_filter() now calculates from/to by group. This allows 'start' and 'end' to be dynamic. Also docs and tests updated

--- R/time_filter.R
+++ R/time_filter.R
@@ -1,6 +1,6 @@
-#' Succinctly filter a `tbl_time` object by date
+#' Succinctly filter a `tbl_time` object by its index
 #'
-#' Use a concise filtering method to filter a `tbl_time` object by it's `index`.
+#' Use a concise filtering method to filter a `tbl_time` object by its `index`.
 #'
 #' @param x A `tbl_time` object.
 #' @param time_formula A period to filter over.
@@ -9,29 +9,35 @@
 #' @details
 #'
 #' The `time_formula` is specified using the format `from ~ to`.
-#' Each side of the `time_formula` is specified as `YYYY-MM-DD + HH:MM:SS`,
-#' but powerful shorthand is available. Some examples are:
-#' * __Year:__ `2013 ~ 2015`
-#' * __Month:__ `2013-01 ~ 2016-06`
-#' * __Day:__ `2013-01-05 ~ 2016-06-04`
-#' * __Second:__ `2013-01-05 + 10:22:15 ~ 2018-06-03 + 12:14:22`
-#' * __Variations:__ `2013 ~ 2016-06`
+#' Each side of the `time_formula` is specified as the character
+#' `'YYYY-MM-DD HH:MM:SS'`, but powerful shorthand is available.
+#' Some examples are:
+#' * __Year:__ `'2013' ~ '2015'`
+#' * __Month:__ `'2013-01' ~ '2016-06'`
+#' * __Day:__ `'2013-01-05' ~ '2016-06-04'`
+#' * __Second:__ `'2013-01-05 10:22:15' ~ '2018-06-03 12:14:22'`
+#' * __Variations:__ `'2013' ~ '2016-06'`
 #'
 #' The `time_formula` can also use a one sided formula.
-#' * __Only dates in 2015:__ `~2015`
-#' * __Only dates March 2015:__ `~2015-03`
+#' * __Only dates in 2015:__ `~'2015'`
+#' * __Only dates March 2015:__ `~'2015-03'`
 #'
-#' All shorthand dates are expanded to be valid POSIXct dates.
+#' The `time_formula` can also use `'start'` and `'end'` as keywords for
+#' your filter.
+#' * __Start of the series to end of 2015:__ `'start' ~ '2015'`
+#' * __Start of 2014 to end of series:__ `'2014' ~ 'end'`
+#'
+#' All shorthand dates are expanded:
 #' * The `from` side is expanded to be the first date in that period
 #' * The `to` side is expanded to be the last date in that period
 #'
-#' This means that the following examples are equivalent:
+#' This means that the following examples are equivalent (assuming your
+#' index is a POSIXct):
 #' * `2015 ~ 2016 == 2015-01-01 + 00:00:00 ~ 2016-12-31 + 23:59:59`
 #' * `~2015 == 2015-01-01 + 00:00:00 ~ 2015-12-31 + 23:59:59`
 #' * `2015-01-04 + 10:12 ~ 2015-01-05 == 2015-01-04 + 10:12:00 ~ 2015-01-05 + 23:59:59`
 #'
 #' This function respects [dplyr::group_by()] groups.
-#'
 #'
 #' @rdname time_filter
 #'
@@ -45,24 +51,27 @@
 #'   dplyr::group_by(symbol)
 #'
 #' # 2013-01-01 to 2014-12-31
-#' time_filter(FANG, 2013 ~ 2014)
+#' time_filter(FANG, '2013' ~ '2014')
 #'
 #' # 2013-05-25 to 2014-06-04
-#' time_filter(FANG, 2013-05-25 ~ 2014-06-04)
+#' time_filter(FANG, '2013-05-25' ~ '2014-06-04')
 #'
 #' # Using the `[` subset operator
-#' FANG[2014~2015]
+#' FANG['2014'~'2015']
 #'
 #' # Using `[` and one sided formula for only dates in 2014
-#' FANG[~2014]
+#' FANG[~'2014']
 #'
 #' # Using `[` and column selection
-#' FANG[2013~2016, c("date", "adjusted")]
+#' FANG['2013'~'2016', c("date", "adjusted")]
 #'
-#' # You can filter with variables
+#' # Variables are unquoted using rlang
 #' lhs_date <- "2013"
 #' rhs_date <- as.Date("2014-01-01")
 #' time_filter(FANG, lhs_date ~ rhs_date)
+#'
+#' # Use the keywords 'start' and 'end' to conveniently access ends
+#' time_filter(FANG, 'start' ~ '2014')
 #'
 #'
 time_filter <- function(x, time_formula) {
@@ -77,28 +86,33 @@ time_filter.default <- function(x, time_formula) {
 #' @export
 time_filter.tbl_time <- function(x, time_formula) {
 
-  index_col  <- get_index_col(x)
   index_quo  <- get_index_quo(x)
+  tz <- get_index_time_zone(x)
 
-  # Parse the time_formula, don't convert to dates yet
-  tf_list <- parse_time_formula(index_col, time_formula)
+  # from/to setup is done inside the call to filter so it is unique to
+  # each group
+  x_filtered <- dplyr::filter(x, {
 
-  # Could allow for multifilter idea here
+    # Parse the time_formula, don't convert to dates yet
+    tf_list <- parse_time_formula(!! index_quo, time_formula)
 
-  # Then convert to datetime
-  from_to <- purrr::map(
-    .x = tf_list,
-    .f = ~list_to_datetime(index_col, .x, tz = get_index_time_zone(x))
-  )
+    # Could allow for multifilter idea here
 
-  # Get sequence creation pieces ready
-  from <- from_to[[1]]
-  to   <- from_to[[2]]
+    # Then convert to datetime
+    from_to <- purrr::map(
+      .x = tf_list,
+      .f = ~list_to_datetime(!! index_quo, .x, tz = tz)
+    )
 
-  # Final assertion of order
-  assert_from_before_to(from, to)
-  
-  x_filtered <- dplyr::filter(x, sorted_range_search(!! index_quo, from, to))
+    # Get sequence creation pieces ready
+    from <- from_to[[1]]
+    to   <- from_to[[2]]
+
+    # Final assertion of order
+    assert_from_before_to(from, to)
+
+    sorted_range_search(!! index_quo, from, to)
+  })
 
   sloop::reconstruct(x_filtered, x)
 }
@@ -162,35 +176,35 @@ time_filter.tbl_time <- function(x, time_formula) {
 #' @export
 # `[.grouped_tbl_time` <- function(x, i, j, drop = FALSE) {
 #   x_tbl <- NextMethod()
-# 
+#
 #   group_names <- dplyr::group_vars(x)
-# 
+#
 #   # If the groups have been removed
 #   if(!all(group_names %in% colnames(x_tbl))) {
-# 
+#
 #     if(inherits(x_tbl, "tbl_time")) {
-# 
+#
 #       as_tbl_time(x_tbl, !! get_index_quo(x_tbl))
-# 
+#
 #     } else {
-# 
+#
 #       as_tibble(x_tbl)
-# 
+#
 #     }
-# 
+#
 #     # If all the groups are still there
 #   } else {
-# 
+#
 #     if(inherits(x_tbl, "tbl_time")) {
-# 
+#
 #       grouped_tbl_time(dplyr::grouped_df(x_tbl, group_names), !! get_index_quo(x_tbl))
-# 
+#
 #     } else {
-# 
+#
 #       dplyr::grouped_df(x_tbl, group_names)
-# 
+#
 #     }
-# 
+#
 #   }
 # }
 
