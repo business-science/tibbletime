f4835304fdbb2b0134a444844b40171cb1caabad
DavisVaughan <mvaugh15@uncc.edu>
2017-12-23 09:37:52 -0500

    Add tests

--- tests/testthat/test_collapse_index.R
+++ tests/testthat/test_collapse_index.R
@@ -28,3 +28,24 @@ test_that("start_date can alter the collapsed index grouping", {
   expect_equal(test,
                as.Date(c("2017-12-02", "2017-12-02", "2017-12-03")))
 })
+
+test_that("Collapsing works on yearmon", {
+  ex <- create_series(~'2017', "monthly", "yearmon")
+
+  expect_equal(collapse_index(ex$date, "yearly"),
+               zoo::as.yearmon(rep(2017.917, 12)))
+})
+
+test_that("Collapsing works on yearqtr", {
+  ex <- create_series(~'2017', "quarter", "yearqtr")
+
+  expect_equal(collapse_index(ex$date, "yearly"),
+               zoo::as.yearqtr(rep(2017.75, 4)))
+})
+
+test_that("Collapsing works on hms", {
+  ex <- create_series(~'12:00', "second", "hms")
+
+  expect_equal(collapse_index(ex$date, "minute", side = "start"),
+               hms::hms(rep(43200, 60)))
+})
