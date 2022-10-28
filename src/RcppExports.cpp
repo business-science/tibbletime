df974442ac723f3c25e82ecfac63a13b58b71e27
DavisVaughan <davis@rstudio.com>
2022-10-28 09:50:21 -0400

    Regenerate Rcpp files

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // is_ordered_numeric
 bool is_ordered_numeric(NumericVector x);
 RcppExport SEXP _tibbletime_is_ordered_numeric(SEXP xSEXP) {
